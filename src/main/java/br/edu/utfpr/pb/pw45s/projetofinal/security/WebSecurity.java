19a5ae90969cf7713deb1f75698a2b0943c7ba5a
Vinicius Pegorini <viniciuspegorini@gmail.com>
2025-11-04 22:38:01 -0300

    ajustes cors

--- src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/security/WebSecurity.java
+++ src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/security/WebSecurity.java
@@ -77,7 +77,7 @@ public class WebSecurity {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://vess.app.pb.utfpr.edu.br"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Origin"));
         configuration.setExposedHeaders(List.of("Authorization"));
